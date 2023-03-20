704b4dfd7c177977537dc8bba060545fb3cf1268
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-03-20 18:36:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const people = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        phone: '111-111-1111',
+        age: 30,
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Poe',
+        email: 'jane@gmail.com',
+        phone: '222-222-2222',
+        age: 25,
+    },
+    {
+        firstName: 'Bob',
+        lastName: 'Foe',
+        email: 'bob@gmail.com',
+        phone: '333-333-3333',
+        age: 45,
+    },
+    {
+        firstName: 'Sara',
+        lastName: 'Soe',
+        email: 'Sara@gmail.com',
+        phone: '444-444-4444',
+        age: 19,
+    },
+    {
+        firstName: 'Jose',
+        lastName: 'Koe',
+        email: 'jose@gmail.com',
+        phone: '555-555-5555',
+        age: 23,
+    },
+];
+
+// Challenge 1
+const youngPeople = people.filter((user) => user.age <= 25).map((user) => {
+    return {
+        name: user.firstName + " " + user.lastName,
+        email: user.email,
+    }
+})
+console.log(youngPeople);
+
+// Challenge 2
+const numbers = [2, -30, 50, 20, -12, -9, 7];
+const positiveSum = numbers.filter((nums) => nums > 0).reduce((acc, posNum) => acc + posNum, 0)
+
+console.log(positiveSum); // 79
+
+// Challenge 3
+const words = ['coder', 'programmer', 'developer'];
+const capitalizeWords = words.map((word) => {
+    return word[0].toUpperCase() + word.slice(1, word.length);
+});
+
+console.log(capitalizeWords); // ['Coder', 'Programmer', 'Developer']
+
+
